c1f99b8d55ae0d89ec06d6ace92bc1fba3d77c3f
rshahmeer27 <shahmeerbhatti27@gmail.com>
2023-04-14 16:43:35 +0500

    Initial

---
+++ src/common/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () =>
+  (
+    <footer className="footer text-center bg-light">
+        <div className="container">
+          <span className="text-muted">Shahmeer's Devops Assignment</span>
+        </div>
+      </footer>
+  )
+
+export default Footer;
